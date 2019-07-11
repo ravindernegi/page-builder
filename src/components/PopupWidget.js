04c23236c96cad2442412e7cf21d1b46fa157d03
Ravinder Negi <r.singh.negi85@gmail.com>
2019-07-11 19:19:27 +0530

    changed state full component to stateless compoents

--- src/components/PopupWidget.js
+++ src/components/PopupWidget.js
@@ -1,14 +1,6 @@
-import React, { Component } from "react";
-
-class PopupWidget extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    return <React.Fragment><h1>PopupWidget</h1></React.Fragment>;
-  }
-}
-
 export default PopupWidget;
+
+function PopupWidget(props) {
+  return <h1>PopupWidget</h1>;
+}
+export default  PopupWidget;
