889532d9f9182b17ef506a6c4960942bd115c16e
Ravinder Negi <r.singh.negi85@gmail.com>
2019-07-22 20:02:05 +0530

    updated container

--- src/components/PopupWidget.js
+++ src/components/PopupWidget.js
@@ -6,6 +6,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 class PopupWidget extends Component {
 	constructor() {
@@ -43,9 +44,9 @@ class PopupWidget extends Component {
         <DialogTitle id="alert-dialog-title">Components</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
-		  <Icon className={classes.icon}>
-          		view_list
-          </Icon>
+            <Grid item xs={8}>
+              
+            </Grid>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
